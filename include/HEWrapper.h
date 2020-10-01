1d636e48d2c413a1bf4d26b375a26f549f566cdd
AaronDou <aaron.yzdou@gmail.com>
2020-10-01 01:43:11 -0700

    finished HEWrapper

--- include/HEWrapper.h
+++ include/HEWrapper.h
@@ -8,6 +8,7 @@
 #include <regex>
 #include <omp.h>
 #include <cmath>
+#include <cassert>
 
 #include "seal/seal.h"
 #include "Util.h"
@@ -15,10 +16,9 @@
 using namespace std;
 using namespace seal;
 
-
 namespace mycryptonets
 {
-      struct AtomicSealBfvEnvironment
+    struct AtomicSealBfvEnvironment
     {
         /*
         * Parameter selections
@@ -93,7 +93,38 @@ namespace mycryptonets
         vector<BigUInt> preComputedCoefficients;
     };
 
-  
+    vector<uint64_t> splitBigNumbers(double num, double scale, const SealBfvEnvironment &env)
+    {
+        size_t envCount = env.environments.size();
+        vector<uint64_t> res(envCount, 0);
+
+        auto w = (long long)round(num * scale);
+        BigUInt z = w < 0 ? env.uIntBigFactor - (uint64_t)abs(w) : BigUInt(to_hex(w));
+
+        for (size_t i = 0; i < envCount; i++)
+        {
+            BigUInt temp;
+            z.divrem(env.factors[i], temp);
+            res[i] = (uint64_t)temp.to_double();
+        }
+
+        return res;
+    }
+
+    double joinSplitNumbers(const vector<uint64_t> &split, const SealBfvEnvironment &env)
+    {
+        BigUInt res("0");
+        for (size_t i = 0; i < env.environments.size(); i++)
+        {
+            res += env.preComputedCoefficients[i] * split[i];
+        }
+        res.divrem(env.uIntBigFactor, res);
+        if (res * 2 > env.uIntBigFactor)
+        {
+            return -1 * (env.uIntBigFactor - res).to_double();
+        }
+        return res.to_double();
+    }
 
     // A wrapper class for Ciphertext with CRT
     struct SealBfvCiphertext
@@ -101,16 +132,65 @@ namespace mycryptonets
         SealBfvCiphertext() = default;
         SealBfvCiphertext(const vector<double> &m,
                           const SealBfvEnvironment &env,
-                          double scaleFactor = 1.0) : scale(scaleFactor)
+                          double scale = 1.0) : batchSize(m.size()), scale(scale)
         {
+            size_t envCount = env.environments.size();
+            vector<vector<uint64_t>> split(envCount, vector<uint64_t>(batchSize, 0));
+            for (size_t i = 0; i < batchSize; i++)
+            {
+                auto temp = splitBigNumbers(m[i], scale, env);
+                for (size_t j = 0; j < envCount; j++)
+                {
+                    split[j][i] = temp[j];
+                }
+            }
+            for (size_t j = 0; j < envCount; j++)
+            {
+                Plaintext temp_p;
+                env.environments[j].batchEncoderPtr->encode(split[j], temp_p);
+                Ciphertext temp_c;
+                env.environments[j].encryptorPtr->encrypt(move(temp_p), temp_c);
+                eVectors.emplace_back(move(temp_c));
+            }
         }
+
+        SealBfvCiphertext(vector<Ciphertext> &eVectors,
+                          double scale,
+                          size_t batchSize) : eVectors(move(eVectors)),
+                                              scale(scale),
+                                              batchSize(batchSize) {}
 
         ~SealBfvCiphertext() {}
 
-        // vector<double> decrypt(const SealBfvEnvironment &env) {}
+        vector<double> decrypt(const SealBfvEnvironment &env)
+        {
+
+            size_t envCount = eVectors.size();
+            vector<vector<uint64_t>> split(batchSize, vector<uint64_t>(envCount, 0));
+
+            for (size_t j = 0; j < envCount; j++)
+            {
+                Plaintext temp_p;
+                env.environments[j].decryptorPtr->decrypt(eVectors[j], temp_p);
+                vector<uint64_t> temp_vec;
+                env.environments[j].batchEncoderPtr->decode(move(temp_p), temp_vec);
+                for (size_t i = 0; i < batchSize; i++)
+                {
+                    split[i][j] = temp_vec[i];
+                }
+            }
+
+            vector<double> res(batchSize, 0.0);
+            for (size_t i = 0; i < batchSize; i++)
+            {
+                res[i] = joinSplitNumbers(split[i], env) / scale;
+            }
+            return res;
+        }
 
         vector<Ciphertext> eVectors;
         double scale;
+        size_t batchSize;
     };
 
     // A wrapper class for Plaintext with CRT
@@ -120,31 +200,86 @@ namespace mycryptonets
 
         SealBfvPlaintext(double m,
                          const SealBfvEnvironment &env,
-                         double scaleFactor = 1.0) : scale(scaleFactor) {}
+                         double scale = 1.0) : scale(scale)
+        {
+            vector<uint64_t> split = splitBigNumbers(m, scale, env);
+            for (auto num : split)
+            {
+                pVectors.emplace_back(to_hex(num));
+            }
+        }
+
+        ~SealBfvPlaintext() {}
 
         vector<Plaintext> pVectors;
         double scale;
     };
 
-    // Maximum number support:
-    vector<uint64_t> splitBigNumbers(double num, double scale, const SealBfvEnvironment &env)
+    void square_inplace(SealBfvCiphertext &ciphertext,
+                        const SealBfvEnvironment &env)
+    {
+
+        for (size_t i = 0; i < env.environments.size(); i++)
+        {
+            env.environments[i].evaluatorPtr->square_inplace(ciphertext.eVectors[i]);
+            env.environments[i].evaluatorPtr->relinearize_inplace(ciphertext.eVectors[i], env.environments[i].relin_keys);
+        }
+        ciphertext.scale *= ciphertext.scale;
+    }
+
+    void multiply_plain(const SealBfvCiphertext &ciphertext,
+                        const SealBfvPlaintext &plaintext,
+                        SealBfvCiphertext &destination,
+                        const SealBfvEnvironment &env)
     {
         size_t envCount = env.environments.size();
-        vector<uint64_t> res{envCount, 0};
-
-        auto w = (long long)round(num * scale);
-        BigUInt z = w < 0 ? env.uIntBigFactor - (uint64_t)abs(w) : BigUInt(to_hex(w));
+        vector<Ciphertext> eVectors(envCount, Ciphertext());
 
         for (size_t i = 0; i < envCount; i++)
         {
-            BigUInt temp;
-            z.divrem(env.factors[i], temp);
-            res.emplace_back(stoul(temp.to_dec_string()));
+            Ciphertext temp;
+            env.environments[i].evaluatorPtr->multiply_plain(ciphertext.eVectors[i],
+                                                             plaintext.pVectors[i],
+                                                             eVectors[i]);
         }
-
-        return res;
+        destination.eVectors = move(eVectors);
+        destination.scale = ciphertext.scale * plaintext.scale;
+        destination.batchSize = ciphertext.batchSize;
     }
 
-    // double joinSplitNumbers() {}
+    void add_many(const vector<SealBfvCiphertext> &ciphertexts,
+                  SealBfvCiphertext &destination,
+                  const SealBfvEnvironment &env)
+    {
+        assert(ciphertexts.size() > 0);
+        //TODO assert all the scales are equal
+
+        size_t envCount = env.environments.size();
+        vector<Ciphertext> eVectors(envCount, Ciphertext());
+
+        for (size_t i = 0; i < envCount; i++)
+        {
+            eVectors[i] = ciphertexts[0].eVectors[i];
+            for (size_t j = 1; j < ciphertexts.size(); j++)
+            {
+                env.environments[i].evaluatorPtr->add_inplace(eVectors[i], ciphertexts[j].eVectors[i]);
+            }
+        }
+        destination.eVectors = move(eVectors);
+        destination.scale = ciphertexts[0].scale;
+        destination.batchSize = ciphertexts[0].batchSize;
+    }
+
+    void add_plain_inplace(SealBfvCiphertext &ciphertext,
+                           const SealBfvPlaintext &plaintext,
+                           const SealBfvEnvironment &env)
+    {
+        assert(ciphertext.scale == plaintext.scale);
+
+        for (size_t i = 0; i < env.environments.size(); i++)
+        {
+            env.environments[i].evaluatorPtr->add_plain_inplace(ciphertext.eVectors[i], plaintext.pVectors[i]);
+        }
+    }
 
 } // namespace mycryptonets
---
+++ test/NeuralNetworksTest.cpp
@@ -0,0 +1,93 @@
+#include <gtest/gtest.h>
+
+#include "NeuralNetworks.h"
+
+using namespace std;
+using namespace seal;
+using namespace mycryptonets;
+
+// class MyCryptoNetsTest : public ::testing::Test
+// {
+
+// protected:
+//     MyCryptoNetsTest()
+//     {
+//         envPtr = make_shared<SealBFVEnvironment>(8192, 549764251649);
+//     }
+
+//     virtual ~MyCryptoNetsTest() {}
+
+//     shared_ptr<SealBFVEnvironment> envPtr;
+// };
+
+// TEST_F(MyCryptoNetsTest, Square)
+// {
+//     vector<vector<uint64_t>> matrix_p{
+//         {3ULL, 1ULL, 2ULL},
+//         {2ULL, 1ULL, 2ULL},
+//         {1ULL, 1ULL, 2ULL},
+//     };
+//     auto encoded = encode(*envPtr, matrix_p);
+//     auto encrypted = encrypt(*envPtr, encoded);
+
+//     square(*envPtr, encrypted);
+
+//     auto decrypted = decrypt(*envPtr, encrypted);
+//     auto decoded = decode(*envPtr, decrypted);
+//     print_matrix(decoded);
+// }
+
+// TEST_F(MyCryptoNetsTest, WeightEncoding)
+// {
+//     vector<vector<uint64_t>> matrix_p{
+//         {3ULL, 1ULL, 2ULL},
+//     };
+//     auto encoded = encode(*envPtr, matrix_p);
+//     auto encrypted = encrypt(*envPtr, encoded);
+
+//     Plaintext p1 {"3"};
+//     envPtr->evaluatorPtr->multiply_plain_inplace(encrypted[0], p1);
+    
+//     auto decrypted = decrypt(*envPtr, encrypted);
+//     auto decoded = decode(*envPtr, decrypted);
+//     print_matrix(decoded);
+// }
+
+// TEST_F(MyCryptoNetsTest, FC)
+// {
+//     vector<vector<uint64_t>> matrix_p{
+//         {3ULL, 1ULL, 2ULL},
+//         {2ULL, 1ULL, 2ULL},
+//         {1ULL, 1ULL, 2ULL},
+//     };
+//     auto encoded = encode(*envPtr, matrix_p);
+//     auto encrypted = encrypt(*envPtr, encoded);
+
+//     Plaintext weight00 {"1"};
+//     Plaintext weight01 {"2"};
+//     Plaintext weight02 {"3"};
+//     Plaintext weight10 {"2"};
+//     Plaintext weight11 {"1"};
+//     Plaintext weight12 {"1"};
+//     vector<Plaintext> weights {weight00, weight01, weight02, weight10, weight11, weight12};
+
+//     Plaintext bias0 {"5"};
+//     Plaintext bias1 {"2"};
+//     vector<Plaintext> biases {bias0, bias1};
+
+//     auto result = fc(*envPtr, encrypted, weights, biases, 3);
+
+//     auto decrypted = decrypt(*envPtr, result);
+//     auto decoded = decode(*envPtr, decrypted);
+//     print_matrix(decoded);
+//     /*
+//     [ 15, 11, 17, 5, ..., 5, 5, 5, 5 ]
+//     [ 11, 6, 10, 2, ..., 2, 2, 2, 2 ]
+//     */
+// }
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
